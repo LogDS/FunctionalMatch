6ab80fcc4593e6a5583108f1cfe37ac1115a791b
Giacomo Bergami <bergamigiacomo@gmail.com>
2025-03-27 07:50:59 +0000

    Update __init__.py

--- FunctionalMatch/__init__.py
+++ FunctionalMatch/__init__.py
@@ -1,4 +1,1 @@
-from FunctionalMatch.functions.Instantiate import instantiate
-from .functions.structural_match import var, JSONPath
-import FunctionalMatch.PropositionalLogic
-from FunctionalMatch.functions.Where import where
+
