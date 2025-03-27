7530088f1206dc601a60589d7e19d6e699db167e
Giacomo Bergami <bergamigiacomo@gmail.com>
2025-03-27 07:43:27 +0000

    Clean branch for external dependencies

--- FunctionalMatch/example/LaSSI/eFOLsemantics/Enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class PairwiseCases(Enum):
-    NonImplying = 0
-    Implying = 1
-    MutuallyExclusive = 2
-    Equivalent = 3
--- main.py
+++ main.py
@@ -139,5 +139,5 @@ def simpler_test():
 
 if __name__ == '__main__':
     # formulae_rewriting_test()
-    # tst_query()
-    simpler_test()
+    tst_query()
+    # simpler_test()
